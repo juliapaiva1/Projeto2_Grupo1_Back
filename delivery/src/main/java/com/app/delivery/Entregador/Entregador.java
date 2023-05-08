97f9852cf44798ac8cb33093feadb1c6ae3074e8
Lucca <104103173+LuccaHiratsuca@users.noreply.github.com>
2023-05-08 03:32:27 -0300

    feat: validation in fields + fix dto

--- delivery/src/main/java/com/app/delivery/Entregador/Entregador.java
+++ delivery/src/main/java/com/app/delivery/Entregador/Entregador.java
@@ -1,8 +1,13 @@
 package com.app.delivery.Entregador;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @Entity
@@ -14,21 +19,30 @@ public class Entregador {
 
     //usar como identifier
     @Column
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, max = 100)
     private String nome;
 
     @Column
+    @NotBlank(message = "CPF é obrigatório")
+    @CPF
     private String cpf;
 
     @Column
+    @NotBlank(message = "Tipo de veículo é obrigatório")
     private String tipo_veiculo;
 
     @Column
     private Double preco_viagem;
 
     @Column
+    @Enumerated(EnumType.STRING)
+    @NotBlank(message = "Status de ocupação é obrigatório")
     private EntregadorOcupacao status_ocupacao;
 
     @Column
+    @Enumerated(EnumType.STRING)
+    @NotBlank(message = "Status de utilização é obrigatório")
     private EntregadorUtilizacao status_utilizacao;
 
 }
--- delivery/src/main/java/com/app/delivery/Entregador/EntregadorDTO.java
+++ delivery/src/main/java/com/app/delivery/Entregador/EntregadorDTO.java
@@ -13,18 +13,14 @@ public class EntregadorDTO {
 
     private Long id;
     private String nome;
-    private String cpf;
     private String tipo_veiculo;
-    private Double preco_viagem;
     private EntregadorOcupacao status_ocupacao;
     private EntregadorUtilizacao status_utilizacao;
 
     public EntregadorDTO (Entregador entregador){
         this.id = entregador.getId();
         this.nome = entregador.getNome();
-        this.cpf = entregador.getCpf();
         this.tipo_veiculo = entregador.getTipo_veiculo();
-        this.preco_viagem = entregador.getPreco_viagem();
         this.status_ocupacao = entregador.getStatus_ocupacao();
         this.status_utilizacao = entregador.getStatus_utilizacao();
     }
